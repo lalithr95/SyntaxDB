3fe40ba16f3ba82d7db351e7fba3a5e4f41abf07
Lalith <lalithr95@gmail.com>
2016-06-13 18:08:56 +0530

    concept apis implementation

---
+++ lib/syntaxdb/concept.rb
@@ -0,0 +1,64 @@
+require 'request'
+
+module Syntaxdb
+  class Concept
+    attr_accessor :concept_id, :concept_permalink
+
+    def initialize(concept_id = nil, permalink = nil)
+      @concept_id = concept_id
+      @concept_permalink = permalink
+    end
+
+    class << self
+
+      def search
+      end
+
+      def all(options = {})
+        Request.send_request(options, '/concepts')
+      end
+
+      def find(options = {})
+        @concept_id = options[:concept_id]
+        if @concept_id
+          options.delete(:concept_id)
+          Request.send_request(options, "/concepts/#{@concept_id}")
+        end
+      end
+
+      def language_concepts(options = {})
+        @concept_permalink = options[:concept_permalink]
+        language_permalink = options[:language_permalink]
+        if language_permalink
+          options.delete(:language_permalink)
+          if @concept_permalink
+            options.delete(:concept_permalink)
+            Request.send_request(options, "/languages/#{language_permalink}/concepts/#{@concept_permalink}")
+          else
+            Request.send_request(options, "/languages/#{language_permalink}/concepts")
+          end
+        end
+      end
+
+      def search_concepts(options = {})
+        language_permalink = options[:language_permalink]
+        if language_permalink
+          options.delete(:language_permalink)
+          Request.send_request(options, "/languages/#{language_permalink}/concepts/search")
+        end
+      end
+
+      def find_category_concepts(options = {})
+        language_permalink = options[:language_permalink]
+        category_id = options[:category_id]
+        if language_permalink && category_id
+          options.delete(:category_id)
+          options.delete(:language_permalink)
+          Request.send_request(options, "/languages/#{language_permalink}/categories/#{category_id}/concepts")
+        end
+      end
+    end
+
+  end
+
+end
--- lib/syntaxdb/language.rb
+++ lib/syntaxdb/language.rb
@@ -3,44 +3,49 @@ require 'request'
 module Syntaxdb
   class Language
     attr_accessor :language_permalink
+
+    def initalize(language_permalink = nil)
+      @language_permalink = language_permalink
+    end
     
-    def all(options = {})
-      Request.send_request(options, '/languages')
-    end
-
-    # params language_permalink
-    def find(options = {})
-      @language_permalink = options[:language_permalink]
-      if @language_permalink
-        options.delete(:language_permalink)
-        Request.send_request(options, "/languages/#{@language_permalink}")
+    class << self
+      def all(options = {})
+        Request.send_request(options, '/languages')
       end
-    end
 
-    # params language_permalink
-    # Find categories by language_permalink
-    def find_categories(options = {})
-      @language_permalink = options[:language_permalink]
-      if @language_permalink
-        options.delete(:language_permalink)
-        Request.send_request(options, "/languages/#{@language_permalink}/categories")
+      # params language_permalink
+      def find(options = {})
+        @language_permalink = options[:language_permalink]
+        if @language_permalink
+          options.delete(:language_permalink)
+          Request.send_request(options, "/languages/#{@language_permalink}")
+        end
       end
-    end
 
-    # Find concepts by language_permalink and category
-    def find_concepts(options = {})
-      @language_permalink = options[:language_permalink]
-      category_id = options[:category_id]
-      if @language_permalink
-        options.delete(:language_permalink)
-        if category_id
-          options.delete(:category_id)
-          Request.send_request(options, "/languages/#{@language_permalink}/categories/#{category_id}/concepts")
-        else
-          Request.send_request(options, "/languages/#{@language_permalink}/concepts")
+      # params language_permalink
+      # Find categories by language_permalink
+      def find_categories(options = {})
+        @language_permalink = options[:language_permalink]
+        if @language_permalink
+          options.delete(:language_permalink)
+          Request.send_request(options, "/languages/#{@language_permalink}/categories")
+        end
+      end
+
+      # Find concepts by language_permalink and category
+      def find_concepts(options = {})
+        @language_permalink = options[:language_permalink]
+        category_id = options[:category_id]
+        if @language_permalink
+          options.delete(:language_permalink)
+          if category_id
+            options.delete(:category_id)
+            Request.send_request(options, "/languages/#{@language_permalink}/categories/#{category_id}/concepts")
+          else
+            Request.send_request(options, "/languages/#{@language_permalink}/concepts")
+          end
         end
       end
     end
-
   end
 end
